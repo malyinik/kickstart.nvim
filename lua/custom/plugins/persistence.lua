7c745994645aadf4be231bb1b710c779be0dd654
malyinik <Djaikmorh2005@gmail.com>
2025-03-29 16:02:22 +0300

    refactor: Change key mappings

--- lua/custom/plugins/persistence.lua
+++ lua/custom/plugins/persistence.lua
@@ -3,22 +3,22 @@ return {
   event = 'BufReadPre', -- this will only start session saving when an actual file was opened
   opts = {
     -- load the session for the current directory
-    vim.keymap.set('n', '<leader>qs', function()
+    vim.keymap.set('n', '<leader>pl', function()
       require('persistence').load()
-    end, { desc = 'Load Session' }),
+    end, { desc = '[P]ersistence [l]oad session' }),
 
     -- select a session to load
-    vim.keymap.set('n', '<leader>qS', function()
+    vim.keymap.set('n', '<leader>pS', function()
       require('persistence').select()
-    end, { desc = '[S]elect Session' }),
+    end, { desc = '[P]ersistence [S]elect session' }),
 
     -- load the last session
-    vim.keymap.set('n', '<leader>ql', function()
+    vim.keymap.set('n', '<leader>pL', function()
       require('perswstence').load { last = true }
-    end, { desc = 'Load [L]ast Sesswon' }),
+    end, { desc = '[P]ersistence load [L]ast session' }),
 
     -- stop Persistence => session won't be saved on exit
-    vim.keymap.set('n', '<leader>qd', function()
+    vim.keymap.set('n', '<leader>pd', function()
       require('persistence').stop()
     end, { desc = 'Stop Persistence' }),
   },
